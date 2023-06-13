63fcd3a3fcf5fe5d4d32046c75419539367d6cce
21.11.4307_C Hanan Eksa Putra <101487571+4307HananPutra@users.noreply.github.com>
2023-06-13 20:41:24 +0700

    Add files via upload

---
+++ TugasWeb/src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+        <header class="py-1 px-4">
+    <nav class="navbar navbar-expand-lg py-4 navbar-light">
+        <div class="container-fluid">
+            <img class="logo" src="../assets/EVOS_Esports_Logo_(SVG)_-_Vector69Com.svg.png" alt="" srcset="" width="50" height="50">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link "  href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              </li>
+            </ul>
+            
+          </div>
+        </div>
+      </nav>
+    </header>
+</template>
+<script setup></script>
